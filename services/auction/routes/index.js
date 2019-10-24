01efceb3cf6787f424d3ce6a57f13e9e50b03430
njnisarg <NJnisarg@gmail.com>
2019-10-24 22:44:21 +0530

    Auth middleware

--- services/auction/routes/index.js
+++ services/auction/routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/index');
+const { AuthorizationMiddleware } = require('../../../lib/authMiddleware');
 
 router.get('/getAllAuctions', controllers.getAllAuctions);
 router.get('/getAuction', controllers.getAuction);
-router.post('/createAuction', controllers.createAuction);
-router.put('/updateAuction', controllers.updateAuction);
-router.delete('/deleteAuction', controllers.deleteAuction);
+router.post('/createAuction', AuthorizationMiddleware, controllers.createAuction);
+router.put('/updateAuction', AuthorizationMiddleware, controllers.updateAuction);
+router.delete('/deleteAuction', AuthorizationMiddleware, controllers.deleteAuction);
 router.get('/getAuctionCategories', controllers.getAuctionCategories);
-router.post('/bid', controllers.bid)
+router.post('/bid', controllers.bid);
 
 module.exports = router;
--- services/auth/controllers/index.js
+++ services/auth/controllers/index.js
@@ -2,7 +2,6 @@ const db = require('../db/index');
 const { response } = require('../../../lib/response');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/authConfig');
 
 const registerUser = async (req, res) => {
 
@@ -17,8 +16,11 @@ const registerUser = async (req, res) => {
     try{
         let result = await db.createUser(params);
         let user = result[0][0];
+
         console.log(user);
+
         let token = jwt.sign({id: user.userId}, authConfig.jwtSecret, {expiresIn: 86400});
+
         response(res, null,"User Created Successfully", {'userId':user.userId, 'username':user.username, 'emailId':user.emailId, 'token': token}, 201);
     }catch(err){
         console.log(err);
@@ -26,26 +28,7 @@ const registerUser = async (req, res) => {
     }
 };
 
-/*const AuthorizationMiddleware = async (req,res,next) => {
-    let token = req.header('Authorization');
-
-    if(!token) {
-        response(res, null, {'error':'Token not provided'}, null, 401);
-    }
-
-    token = token.split(' ')[1];
-    console.log(token);
-
-    jwt.verify(token, authConfig.jwtSecret, async (err, decoded) => {
-        if(err){
-            response(res, null, {'error':'Token not verified'}, null, 401);
-        }
-
-
-    });
-};*/
 
 module.exports = {
-    registerUser,
-    //AuthorizationMiddleware
+    registerUser
 };
